3389aeccc82910d708ccc51ebbf64dcec70aa104
이재호 <leejaeho@ijaehos-MacBook-Air.local>
2023-05-23 09:46:27 +0900

    isSpam function to function constructor

--- isSpam.js
+++ isSpam.js
@@ -1,7 +1,30 @@
-import fetch from "node-fetch";
+import fetch, { Request } from "node-fetch";
 
-function isSpam(content, spamLinkDomains, redirectionDepth) {
-	fetch(content).then(console.log);
-}
+const isSpam = (function () {
+	function isSpam(content, spamLinkDomains, redirectionDepth) {
+		this.content = content;
+		this.spamLinkDomains = spamLinkDomains;
+		this.redirectionDepth = redirectionDepth;
+	}
 
-isSpam("https://moiming.page.link/exam?_imcp=1");
+	isSpam.prototype = {
+		constructor: isSpam,
+
+		test() {
+			// const request = new Request(content, { redirect: "manual" });
+			const request = new Request(this.content);
+
+			fetch(request)
+				.then((res) => {
+					console.log(res.status);
+					return res;
+				})
+				.then((res) => res.text())
+				.then(console.log);
+		},
+	};
+
+	return isSpam;
+})();
+
+new isSpam("https://moiming.page.link/exam?_imcp=1").test();
